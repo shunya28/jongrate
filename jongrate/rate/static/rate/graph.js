facc540aa2ee8cc82fd105022026c4e038559838
shunya28 <hakw4morta@gmail.com>
2022-07-30 21:41:24 +0900

    グラフ描画機能を追加

---
+++ jongrate/rate/static/rate/graph.js
@@ -0,0 +1,30 @@
+const ctx = document.getElementById('chart').getContext('2d');
+
+const data = {
+    labels: ["1月", "2月", "3月", "4月", "5月", "6月", "7月"],
+    datasets: [{
+        label: "初めてのデータセット",
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: [0, 10, 5, 2, 20, 30, 45],
+    }]
+};
+
+const options = {
+    responsive: true,
+    plugins: {
+        legend: {
+            position: 'top',
+        },
+        title: {
+            display: true,
+            text: '森レーティング'
+        }
+    }
+};
+
+const lineChart = new Chart(ctx, {
+    type: 'line',
+    data: data,
+    options: options,
+});
